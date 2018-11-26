f36224d7074f12c40ed31d6dbde1d8eea4fe0f53
vincetroid <vince_trance@hotmail.com>
2018-11-26 17:56:30 -0600

    Initial commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import AppButton from "./application/components/AppButton";
+import PreLoader from './application/components/PreLoader';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <PreLoader/>
+    );
+  }
+}
